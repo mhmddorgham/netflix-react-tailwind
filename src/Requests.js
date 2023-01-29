8a032785e32c4e221fc099a09558058258afe62c
mhmddorgham <mohammed.dorgham25@gmail.com>
2023-01-29 15:21:51 +0400

    Get API from moviesDB

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = '5fb50eb9f0bbb3958ce53e3cc57dc9fe';
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+  requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests
