e435cc367ecf7e2add46c40a5bf3871bb48eaab8
mhmddorgham <mohammed.dorgham25@gmail.com>
2023-02-02 23:25:26 +0400

    Add Auth Functions

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/pages/Home'
+import { AuthContextProvider } from './Context/AuthContext'
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-      </Routes>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+        </Routes>
+      </AuthContextProvider>
     </>
   )
 }
