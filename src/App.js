1198a4fd2a6b53685123dae56d75739df032dc79
mhmddorgham <mohammed.dorgham25@gmail.com>
2023-02-05 19:19:22 +0400

    Add pages to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Account from './components/pages/Account'
 import Home from './components/pages/Home'
+import LogIn from './components/pages/LogIn'
+import SignUp from './components/pages/SignUp'
 import { AuthContextProvider } from './Context/AuthContext'
 
 const App = () => {
@@ -11,6 +14,9 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" exact element={<Home />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/account" element={<Account />} />
         </Routes>
       </AuthContextProvider>
     </>
