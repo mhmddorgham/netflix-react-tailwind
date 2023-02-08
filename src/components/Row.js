6b8e9ca3be3a64f04ced64a2e0982e41d1c7a70e
mhmddorgham <mohammed.dorgham25@gmail.com>
2023-02-08 21:25:59 +0400

    Edit Row

--- src/components/Row.js
+++ src/components/Row.js
@@ -26,7 +26,6 @@ const Row = ({ title, fetchURL, rowID }) => {
     var slider = document.getElementById('slider' + rowID);
     slider.scrollLeft = slider.scrollLeft + 500
   }
-  console.log(movies);
   return (
     <>
       <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
